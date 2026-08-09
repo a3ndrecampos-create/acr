f9591d7c6b7b4a27bd26f681db3c947db02f55e0
a3ndrecampos-create <a3ndrecampos@gmail.com>
2026-08-09 17:53:36 -0300

    qrscangera.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.qrscangera.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 6
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
