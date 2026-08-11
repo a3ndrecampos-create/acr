8eb1e4c21038507c5cb476640b1197a1dc0fb525
andre <a3ndrecampos@gmail.com>
2026-08-11 18:35:35 -0300

    fix: atualiza Kotlin para 2.2.20 (exigido pelo Billing 8.3.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
 }
 
@@ -83,9 +84,6 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
     }
 
     packaging {
