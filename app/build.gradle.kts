fd00b699eef53b75e80164668d5b72be597c110d
a3ndrecampos-create <a3ndrecampos@gmail.com>
2026-08-11 00:03:15 -0300

    Update version code and name in build.gradle.kts 1.7.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.qrscangera.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.8"
+        versionCode = 7
+        versionName = "1.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
