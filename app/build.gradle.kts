89c40ca3549fb522cae2d8d421b95056a0be4dfd
a3ndrecampos-create <a3ndrecampos@gmail.com>
2026-08-10 22:17:12 -0300

    Update version code and name to 8 and 1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.qrscangera.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.7"
+        versionCode = 8
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
