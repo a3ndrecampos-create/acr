29004d9406a847477c9c768793a1ed8e64c192b0
a3ndrecampos-create <a3ndrecampos@gmail.com>
2026-08-14 10:12:17 -0300

    Bump version code to 17 and version name to 1.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.qrscangera.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.8.0"
+        versionCode = 17
+        versionName = "1.8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
